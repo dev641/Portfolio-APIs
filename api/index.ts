3821dd79d6c14f45f236bec40543f24080d360f0
Devashish <dev641science@gmail.com>
2024-07-20 22:24:42 +0530

    vercel

--- api/index.ts
+++ api/index.ts
@@ -1,11 +1,11 @@
-// import app from '../src/server'
+import app from '../src/server'
 
-// export default app
-const express = require('express')
-const app = express()
+export default app
+// const express = require('express')
+// const app = express()
 
-app.get('/', (req, res) => res.send('Express on Vercel'))
+// app.get('/', (req, res) => res.send('Express on Vercel'))
 
-app.listen(3000, () => console.log('Server ready on port 3000.'))
+// app.listen(3000, () => console.log('Server ready on port 3000.'))
 
-module.exports = app
+// module.exports = app
