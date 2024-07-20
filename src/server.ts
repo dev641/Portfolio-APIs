8b7a740ba6e75a884d33bb279ea4ae06f959fbcc
Devashish <dev641science@gmail.com>
2024-07-20 22:18:04 +0530

    vercel

--- src/server.ts
+++ src/server.ts
@@ -1,26 +1,18 @@
-// import express, { Express, Request, Response } from 'express'
-// import dotenv from 'dotenv'
+import express, { Express, Request, Response } from 'express'
+import dotenv from 'dotenv'
 
-// dotenv.config()
+dotenv.config()
 
-// const app: Express = express()
-// const port = process.env.PORT || 3000
+const app: Express = express()
+const port = process.env.PORT || 3000
 
-// app.get('/', (_: Request, res: Response) => {
-//   console.log('hello')
-//   res.send('Express + TypeScript Server')
-// })
+app.get('/', (_: Request, res: Response) => {
+  console.log('hello')
+  res.send('Express + TypeScript Server')
+})
 
-// app.listen(port, () => {
-//   console.log(`[server]: Server is running at http://localhost:${port}`)
-// })
+app.listen(port, () => {
+  console.log(`[server]: Server is running at http://localhost:${port}`)
+})
 
-// export default app
-const express = require('express')
-const app = express()
-
-app.get('/', (_: Request, res: Response) => res.send('Express on Vercel'))
-
-app.listen(3000, () => console.log('Server ready on port 3000.'))
-
-module.exports = app
+export default app
