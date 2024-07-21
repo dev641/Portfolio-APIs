ade5351d051bbaee80fb9296ca1414e875367962
Devashish <dev641science@gmail.com>
2024-07-21 17:07:19 +0530

    vercel

--- starter.js
+++ starter.js
@@ -2,5 +2,7 @@ import { register } from 'node:module'
 import { pathToFileURL } from 'node:url'
 
 register('ts-node/esm', pathToFileURL('./'))
-
-import('./src/server.ts')
+;// starter.js
+(async () => {
+  await import('./api/index.ts')
+})()
