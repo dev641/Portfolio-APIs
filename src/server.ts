94756aae52fe0581580b5b9a4c95a28aadf7fc2a
Devashish <dev641science@gmail.com>
2024-07-21 20:44:24 +0530

    Added Different Middleware Setups

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,48 @@
-import express, { Request, Response } from 'express'
+import express, { Request, Response, NextFunction } from 'express'
+import dotenv from 'dotenv'
+import cors from 'cors'
+import helmet from 'helmet'
+import morgan from 'morgan'
+import compression from 'compression'
+
+dotenv.config()
 
 const app = express()
 app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
-// get post routes
+// Enable CORS
+app.use(cors())
+
+// Security headers
+app.use(helmet())
+
+// HTTP request logger
+app.use(morgan('combined'))
+
+// Compress response bodies
+app.use(compression())
+
+// Dummy data for posts
+const posts = [
+  { id: 1, title: 'First Post', content: 'This is the first post' },
+  { id: 2, title: 'Second Post', content: 'This is the second post' }
+]
+
+// Get post routes
 app.get('/post', (_: Request, res: Response) => {
-  res.status(200).json({ message: 'post routes' })
+  res.status(200).json(posts)
 })
 
-// root routes
+// Root route
 app.get('/', (_: Request, res: Response) => {
   res.status(200).json({ message: 'Hello World' })
+})
+
+// Error handling middleware
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  console.error(err.stack)
+  res.status(500).json({ message: err.message })
 })
 
 export default app
